e06e64803bd3039ad05f45f04073c79e80a3e4b1
Yana Mkrtchyan <yana@Yanas-MacBook-Pro.local>
2021-03-27 15:19:05 -0600

    password length

---
+++ Develop/script.js
@@ -0,0 +1,41 @@
+// Assignment code here
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN prompted for character types to include in the password
+// THEN I choose lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+var pwLength = window.prompt('How long should your password be? Enter a number between 8 and 128.');
+
+if (pwLength >= 8 && pwLength <= 128) {
+  return("Your password will be " + pwLength + " characters long.");
+} else { 
+  return("Please enter a number between 8 and 128.");
+}
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
